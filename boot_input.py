a661233546ea6cf7fbf87bbf9b621333e273e38e
Alex Ethier <aethier@gmail.com>
2021-11-14 03:01:18 -0500

    Updated input prompt to handle backspace.

---
+++ boot_input.py
@@ -0,0 +1,19 @@
+from zompt.api.input_prompt import InputPrompt
+import sys
+
+class Boot:
+
+  def __init__(self):
+    pass;
+
+  def boot(self):
+
+    sys.stdout.write("Example prompt: ")
+    sys.stdout.flush()
+    input_prompt = InputPrompt()
+    result = input_prompt.run()
+    print("Result: " + result)
+
+if __name__ == "__main__":
+  boot = Boot()
+  boot.boot()
--- zompt/api/input_prompt.py
+++ zompt/api/input_prompt.py
@@ -1,10 +1,11 @@
 import tty
 import sys
 import termios
+import os
 
 class InputPrompt:
 
-  def __init__(self, options):
+  def __init__(self):
 
     self._orig_settings = termios.tcgetattr(sys.stdin)
     tty.setcbreak(sys.stdin)
@@ -21,16 +22,22 @@ class InputPrompt:
 
   def run(self):
 
-    self._write()
-
     try:
       while True:
+        sys.stdout.flush()
+
         input_char = x=sys.stdin.read(1)[0]
         input_ord = ord(input_char)
+        #print("AE: " + input_char + str(input_ord))
         if(input_ord == 10):
+          sys.stdout.write(os.linesep)
           return "".join(self._input_chars)
         elif(input_ord == 27):
           pass
+        elif(input_ord == 127):
+          if(len(self._input_chars) > 0):
+            self._input_chars.pop()
+            sys.stdout.write('\b \b')
         else:
           sys.stdout.write(input_char)
           self._input_chars.append(input_char)
