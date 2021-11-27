2294036ab9726f0f1034d3c3107774c00abec482
Alex Ethier <aethier@gmail.com>
2021-11-26 23:32:08 -0500

    Updated readme and setup file.

--- setup.py
+++ setup.py
@@ -15,10 +15,16 @@ for root, dirs, files in os.walk("." + os.path.sep + project_name, topdown=True,
 #print(found_packages)
 #print(found_package_dirs)
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
   name=project_name,
   version='0.0.6',
   description='Simple Python Prompt',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   url='https://github.com/alexethier/zompt',
   author='Alex Ethier',
   author_email='aethier@gmail.com',
